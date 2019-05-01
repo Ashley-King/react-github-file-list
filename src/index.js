a2ac34ebc3b0dababb62a41b7c1e2e24a16bf202
Ashley King <ashleyot@gmail.com>
2019-05-01 10:24:47 -0400

    finished git file list mock up and added a practice button to increment in console.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,87 @@
 import React from 'react';
-import moment from 'moment'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom';
 import './index.css';
+import Time from './time'
+
+
 
 const FileList = ({files}) => (
-    <table className="file-list">
+    <div>
+         <table className="file-list">
         <tbody>
             {files.map(file => (
-                <tr className="file-list-item" key={file.id}>
-                    <td className="fileName">{file.name}</td>
-                </tr>
+                <FileListItem key={file.id} file={file}/>
             ))}
         </tbody>
     </table>
+            <Parent/>
+    </div>
+    
+   
 );
 
 FileList.propTypes = {
     files: PropTypes.array
 };
+
+const FileListItem = ({file}) => (
+    <tr className="file-list-item">
+       <FileName file={file}/>
+       <CommitMessage commit={file.latestCommit}/>
+       <td className="age">
+        <Time time={file.updated_at}/>
+       </td>
+    </tr>
+)
+
+FileListItem.propTypes = {
+    file: PropTypes.object.isRequired
+}
+
+
+
+const FileIcon = ({file}) => {
+    let icon = 'fa-file-alt';
+    if(file.type === 'folder'){
+        icon = 'fa-folder'
+    }
+
+    return (
+        <td className="file-icon">
+            <i className={`fa ${icon}`}/>
+        </td>
+    )
+}
+
+FileIcon.propTypes = {
+    files: PropTypes.object.isRequired
+}
+
+const FileName = ({file}) => {
+    return(
+        <React.Fragment>
+            <FileIcon file={file} />
+            <td className="file-name">
+                {file.name}
+            </td>
+        </React.Fragment>
+    )
+}
+
+FileName.propTypes = {
+    file: PropTypes.object.isRequired
+}
+
+const CommitMessage = ({commit }) => (
+    <td className="commit-message">
+        {commit.message}
+    </td>
+)
+
+CommitMessage.propTypes = {
+    commit: PropTypes.object.isRequired
+}
 
 const testFiles = [
     {
@@ -51,6 +114,16 @@ const testFiles = [
         }
     },
 ];
+
+function handleAction(event) { console.log('Child did:', event);
+}
+function Parent() { return (
+<Child onAction={handleAction}/> );
+}
+function Child({ onAction }) { return (
+<button onClick={onAction}> Click Me!
+</button>
+); }
 
 
 ReactDOM.render(<FileList files={testFiles}/>, document.getElementById('root'));
